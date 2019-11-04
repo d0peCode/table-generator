a42ae38604a5f56937fe90a75374ee880b12489f
Borys Tymiński <tyminski.borys@gmail.com>
2019-11-04 06:25:54 +0100

    add hello.vue html markup

--- src/components/hello.vue
+++ src/components/hello.vue
@@ -1,3 +1,6 @@
 <template>
-    <div>Hello</div>
+    <header class="hello">
+        <h1 class="heading-primary">Table Generator</h1>
+        <p>Application which generates table. Made by tymdev for Koleo.</p>
+    </header>
 </template>
---
+++ src/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <main>
+        <hello></hello>
+    </main>
+</template>
+<script>
+import hello from '../components/hello';
+
+export default {
+    components: { hello }
+}
+</script>
