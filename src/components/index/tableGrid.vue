4666fedca9e6ed26460c3ef3149dfcee792491dd
Borys Tymiński <tyminski.borys@gmail.com>
2019-11-04 08:33:20 +0100

    add tableGrid markup, css and functionality

--- src/components/index/tableGrid.vue
+++ src/components/index/tableGrid.vue
@@ -1,5 +1,69 @@
 <template>
-    <section>
-        <h2>Wybierz wielkość</h2>
+    <section class="table-grid" v-if="!tableGenerated">
+        <h2 class="heading-secondary">Wybierz wielkość nowej tabeli</h2>
+        <p>Maksymalna wielkość tabeli:</p>
+        <div class="table-grid__form">
+            <label for="x">od (x)</label>
+            <input id="x" type="number" v-model="gridSize.max.x" min="1" class="table-grid__input">
+            <label for="y">do (y)</label>
+            <input id="y" type="number" v-model="gridSize.max.y" min="1" class="table-grid__input">
+        </div>
+        <p>Najedź i kliknij aby wybrać wielkość nowej tabeli:</p>
+        <div class="table-grid__grid">
+            <div class="table-grid__grid-row"
+                 v-for="n in parseInt(gridSize.max.y)"
+                 :key="n">
+                <div class="table-grid__grid-col"
+                     v-for="i in parseInt(gridSize.max.x)"
+                     @mouseover="selectHovered(n, i)"
+                     @click="selectNewTableSize(n, i)"
+                     :key="i"
+                     :class="n <= gridSize.hovered.x && i <= gridSize.hovered.y
+                                ? 'table-grid__grid-col--hover'
+                                : ''">
+                    {{ n + 'x' + i }}
+                </div>
+            </div>
+        </div>
     </section>
 </template>
+<script>
+import { eventBus } from '../../helpers/eventBus';
+
+export default {
+    data() {
+        return {
+            tableGenerated: false,
+            gridSize: {
+                max: {
+                    x: 10,
+                    y: 10
+                },
+                hovered: {
+                    x: 0,
+                    y: 0
+                },
+                selected: {
+                    x: 0,
+                    y: 0
+                }
+            }
+        }
+    },
+    methods: {
+        generateTable() {
+            eventBus.$emit('generate::table', this.gridSize.selected);
+            this.tableGenerated = true;
+        },
+        selectHovered(x, y) {
+            this.gridSize.hovered.x = x;
+            this.gridSize.hovered.y = y;
+        },
+        selectNewTableSize(x, y) {
+            this.gridSize.selected.x = x;
+            this.gridSize.selected.y = y;
+            this.generateTable();
+        }
+    }
+}
+</script>
