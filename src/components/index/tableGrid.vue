6a794b0ffc95c79a512783d867ba8fdb9a3859aa
Borys Tymiński <tyminski.borys@gmail.com>
2019-11-04 06:34:42 +0100

    add tableGrid component

---
+++ src/components/index/tableGrid.vue
@@ -0,0 +1,5 @@
+<template>
+    <section>
+        <h2>Wybierz wielkość</h2>
+    </section>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,14 @@
 <template>
     <main>
         <hello></hello>
+        <table-grid></table-grid>
     </main>
 </template>
 <script>
-import hello from '../components/hello';
+import hello from '../components/index/hello';
+import tableGrid from '../components/index/tableGrid';
 
 export default {
-    components: { hello }
+    components: { hello, tableGrid }
 }
 </script>
