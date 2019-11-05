faa3071dc0325bae1a0efc1b409a436647362c55
Borys Tymiński <tyminski.borys@gmail.com>
2019-11-05 16:10:14 +0100

    extract tools from tableGenerator component

--- src/components/index/table/TableGenerator.vue
+++ src/components/index/table/TableGenerator.vue
@@ -1,14 +1,5 @@
 <template>
-    <section class="table-generator">
-        <h2 class="heading-secondary">Twoja tabela</h2>
-        <p>Użyj + / - aby dodać / usunąć kolumnę / wiersz:</p>
-        <div class="table-generator__manager">
-            <div class="table-generator__plus" @click="modifyTable('column', 'increase')"></div>
-            <div class="table-generator__minus" @click="modifyTable('column', 'decrease')"></div>
-            <p>{{ tableSize.x }} x {{ tableSize.y }}</p>
-            <div class="table-generator__plus" @click="modifyTable('row', 'increase')"></div>
-            <div class="table-generator__minus" @click="modifyTable('row', 'decrease')"></div>
-        </div>
+    <div class="table-generator">
         <table class="table-generator__table">
             <tr v-for="n in parseInt(tableSize.y) + 1"
                 :key="n">
@@ -26,7 +17,7 @@
                 </td>
             </tr>
         </table>
-    </section>
+    </div>
 </template>
 <script>
 import { eventBus } from "../../../helpers/eventBus";
@@ -46,43 +37,14 @@ export default {
             this.tableValues.push([]);
         }
         eventBus.$on('generate::table', data => {
-            this.called = true;
-            this.tableSize.x = data.x;
-            this.tableSize.y = data.y;
-            for (let i = 0, l = data.y; i < l; i++) {
-                this.tableValues.push([]);
-            }
+            this.tableSize = {x: data.x, y: data.y };
+            for (let i = 0, l = data.y; i < l; i++) this.tableValues.push([]);
         })
     },
     methods: {
         countDecimal(value) {
             if (Math.floor(value) === value) return 0;
             return value.toString().split(".")[1].length || 0;
-        },
-        modifyTable(line, option) {
-            if(line === 'row') {
-                if(option === 'increase') {
-                    this.tableSize.y += 1;
-                    this.tableValues.push([]);
-                }
-                if(option === 'decrease') {
-                    if(this.tableSize.y === 1) return;
-                    this.tableSize.y -= 1;
-                    this.tableValues.pop();
-                }
-            }
-            if(line === 'column') {
-                if(option === 'increase') {
-                    this.tableSize.x += 1;
-                }
-                if(option === 'decrease') {
-                    if(this.tableSize.x === 1) return;
-                    this.tableSize.x -= 1;
-                    for(let i = 0, l = this.tableValues.length; i < l; i++) {
-                        this.tableValues[i].pop();
-                    }
-                }
-            }
         },
         getAverage(array) {
             let total = 0;
---
+++ src/components/index/table/TableTool.vue
@@ -0,0 +1,44 @@
+<template>
+    <section>
+        <h2 class="heading-secondary">Twoja tabela</h2>
+        <p>Użyj + / - aby dodać / usunąć kolumnę / wiersz:</p>
+        <div class="table-generator__manager">
+            <div class="table-generator__plus" @click="modifyTable('column', 'increase')"></div>
+            <div class="table-generator__minus" @click="modifyTable('column', 'decrease')"></div>
+            <p>{{ tableSize.x }} x {{ tableSize.y }}</p>
+            <div class="table-generator__plus" @click="modifyTable('row', 'increase')"></div>
+            <div class="table-generator__minus" @click="modifyTable('row', 'decrease')"></div>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+    methods: {
+        modifyTable(line, option) {
+            if(line === 'row') {
+                if(option === 'increase') {
+                    this.tableSize.y += 1;
+                    this.tableValues.push([]);
+                }
+                if(option === 'decrease') {
+                    if(this.tableSize.y === 1) return;
+                    this.tableSize.y -= 1;
+                    this.tableValues.pop();
+                }
+            }
+            if(line === 'column') {
+                if(option === 'increase') {
+                    this.tableSize.x += 1;
+                }
+                if(option === 'decrease') {
+                    if(this.tableSize.x === 1) return;
+                    this.tableSize.x -= 1;
+                    for(let i = 0, l = this.tableValues.length; i < l; i++) {
+                        this.tableValues[i].pop();
+                    }
+                }
+            }
+        },
+    }
+}
+</script>
